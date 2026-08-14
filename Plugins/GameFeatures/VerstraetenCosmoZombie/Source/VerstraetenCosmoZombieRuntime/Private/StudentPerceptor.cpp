348abe9936e6786e3409cacedc12df9b5a1be505
Cosmo-Verstraeten <cosmo.verstraeten@student.howest.be>
2026-08-14 05:06:54 +0200

    moved file location + added some detection messages

---
+++ Plugins/GameFeatures/VerstraetenCosmoZombie/Source/VerstraetenCosmoZombieRuntime/Private/StudentPerceptor.cpp
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StudentPerceptor.h"
+
+
+UStudentPerceptor::UStudentPerceptor()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UStudentPerceptor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (auto PerceptionComp = GetOwner()->GetComponentByClass<UAIPerceptionComponent>())
+	{
+		PerceptionComp->OnTargetPerceptionUpdated.AddDynamic(this, &UStudentPerceptor::OnPerceptionUpdated);
+	}
+}
+
+void UStudentPerceptor::OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
+{
+	/*GEngine->AddOnScreenDebugMessage(5, 1.f, FColor::Green, 
+	FString::Printf(TEXT("Saw Something!")));*/
+	
+	/*GEngine->AddOnScreenDebugMessage(
+	-1,
+	2.f,
+	FColor::Green,
+	FString::Printf(TEXT("Perceived: %s"), *Actor->GetName()));*/
+	
+	
+	if (!Actor)
+	{
+		return;
+	}
+
+	if (Stimulus.WasSuccessfullySensed())
+	{
+		
+		GEngine->AddOnScreenDebugMessage(
+			-1,
+			2.f,
+			FColor::Green,
+			FString::Printf(TEXT("Detected: %s"), *Actor->GetName())
+		);
+	}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(
+			-1,
+			2.f,
+			FColor::Red,
+			FString::Printf(TEXT("Lost: %s"), *Actor->GetName())
+		);
+	}
+}
