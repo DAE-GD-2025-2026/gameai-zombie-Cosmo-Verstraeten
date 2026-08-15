0c1051b5f73e2a46150cb445ee358e18bf344f36
Cosmo-Verstraeten <cosmo.verstraeten@student.howest.be>
2026-08-15 07:32:01 +0200

    added that the survivor "scans" the house when arriving there

---
+++ Plugins/GameFeatures/VerstraetenCosmoZombie/Source/VerstraetenCosmoZombieRuntime/Private/BTTask_SearchHouse.cpp
@@ -0,0 +1,82 @@
+﻿#include "BTTask_SearchHouse.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTTask_SearchHouse::UBTTask_SearchHouse()
+{
+	NodeName = TEXT("Search House");
+
+	bNotifyTick = true;
+	bCreateNodeInstance = true;
+}
+
+
+EBTNodeResult::Type UBTTask_SearchHouse::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController* AIController =
+		OwnerComp.GetAIOwner();
+
+	if (!AIController)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	APawn* Pawn =
+		AIController->GetPawn();
+
+	if (!Pawn)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	DegreesRotated = 0.f;
+
+	return EBTNodeResult::InProgress;
+}
+
+
+void UBTTask_SearchHouse::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	AAIController* AIController = OwnerComp.GetAIOwner();
+
+	if (!AIController)
+	{
+		FinishLatentTask(OwnerComp, EBTNodeResult::Failed);
+
+		return;
+	}
+
+	APawn* Pawn =
+		AIController->GetPawn();
+
+	if (!Pawn)
+	{
+		FinishLatentTask(OwnerComp, EBTNodeResult::Failed);
+
+		return;
+	}
+
+
+	const float RotationThisFrame = RotationSpeed * DeltaSeconds;
+
+
+	Pawn->AddActorWorldRotation(
+		FRotator(0.f,RotationThisFrame,0.f)
+	);
+
+	DegreesRotated +=FMath::Abs(RotationThisFrame);
+
+
+	if (DegreesRotated >= 360.f)
+	{
+		FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+	}
+}
+
+EBTNodeResult::Type UBTTask_SearchHouse::AbortTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	DegreesRotated = 0.f;
+
+	return EBTNodeResult::Aborted;
+}
---
+++ Plugins/GameFeatures/VerstraetenCosmoZombie/Source/VerstraetenCosmoZombieRuntime/Public/BTTask_SearchHouse.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_SearchHouse.generated.h"
+
+UCLASS()
+class VERSTRAETENCOSMOZOMBIERUNTIME_API UBTTask_SearchHouse : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UBTTask_SearchHouse();
+
+protected:
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+	virtual EBTNodeResult::Type AbortTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+private:
+
+	float RotationSpeed{ 180.f };
+
+	float DegreesRotated{ 0.f };
+};
