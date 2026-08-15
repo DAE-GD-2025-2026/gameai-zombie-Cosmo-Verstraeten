252411e1ea5d82a84b127f8188191e8c10eabf0c
Cosmo-Verstraeten <cosmo.verstraeten@student.howest.be>
2026-08-15 06:00:57 +0200

    made a behaviour tree task for the wander behaviour

---
+++ Plugins/GameFeatures/VerstraetenCosmoZombie/Source/VerstraetenCosmoZombieRuntime/Private/BTTask_Wander.cpp
@@ -0,0 +1,77 @@
+﻿#include "BTTask_Wander.h"
+
+#include "AIController.h"
+#include "SurvivorSteering.h"
+
+UBTTask_Wander::UBTTask_Wander()
+{
+	NodeName = TEXT("Wander");
+}
+
+EBTNodeResult::Type UBTTask_Wander::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+
+	
+	AAIController* AIController = OwnerComp.GetAIOwner();
+
+	if (!AIController)
+	{
+		
+		return EBTNodeResult::Failed;
+	}
+
+	APawn* Pawn = AIController->GetPawn();
+
+	if (!Pawn)
+	{
+		
+		return EBTNodeResult::Failed;
+	}
+
+	USurvivorSteering* SteeringComponent = Pawn->FindComponentByClass<USurvivorSteering>();
+
+	if (!SteeringComponent)
+	{
+
+		return EBTNodeResult::Failed;
+	}
+
+	
+	GEngine->AddOnScreenDebugMessage(
+		-1,
+		5.f,
+		FColor::Green,
+		TEXT("STARTING WANDER")
+	);
+	
+	SteeringComponent->StartWander();
+
+	return EBTNodeResult::InProgress;
+}
+
+
+EBTNodeResult::Type UBTTask_Wander::AbortTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController* AIController = OwnerComp.GetAIOwner();
+
+	if (!AIController)
+	{
+		return EBTNodeResult::Aborted;
+	}
+
+	APawn* Pawn = AIController->GetPawn();
+
+	if (!Pawn)
+	{
+		return EBTNodeResult::Aborted;
+	}
+
+	USurvivorSteering* SteeringComponent = Pawn->FindComponentByClass<USurvivorSteering>();
+
+	if (SteeringComponent)
+	{
+		SteeringComponent->StopSteering();
+	}
+
+	return EBTNodeResult::Aborted;
+}
---
+++ Plugins/GameFeatures/VerstraetenCosmoZombie/Source/VerstraetenCosmoZombieRuntime/Public/BTTask_Wander.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_Wander.generated.h"
+
+UCLASS()
+class VERSTRAETENCOSMOZOMBIERUNTIME_API UBTTask_Wander : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UBTTask_Wander();
+
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+	virtual EBTNodeResult::Type AbortTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	
+};
