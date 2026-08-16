4a37def581b367285ccb7878e0299a075ba01095
Cosmo-Verstraeten <cosmo.verstraeten@student.howest.be>
2026-08-16 19:17:25 +0200

    added the ability to select an enemy of all the seen ones
    for now just the closest one

---
+++ Plugins/GameFeatures/VerstraetenCosmoZombie/Source/VerstraetenCosmoZombieRuntime/Private/BTTask_SelectEnemy.cpp
@@ -0,0 +1,86 @@
+﻿#include "BTTask_SelectEnemy.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Pawn.h"
+
+#include "StudentPerceptor.h"
+
+#include "Zombies/BaseZombie.h"
+
+
+UBTTask_SelectEnemy::UBTTask_SelectEnemy()
+{
+	NodeName = TEXT("Select Enemy");
+	
+	TargetEnemyKey.AddObjectFilter(
+		this,
+		GET_MEMBER_NAME_CHECKED(
+			UBTTask_SelectEnemy,
+			TargetEnemyKey),
+		AActor::StaticClass());
+}
+
+
+EBTNodeResult::Type UBTTask_SelectEnemy::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController* AIController = OwnerComp.GetAIOwner();
+
+	if (!AIController)
+	{
+		return EBTNodeResult::Failed;
+	}
+	
+	APawn* Pawn =AIController->GetPawn();
+
+	if (!Pawn)
+	{
+		return EBTNodeResult::Failed;
+	}
+	
+	UBlackboardComponent* Blackboard =OwnerComp.GetBlackboardComponent();
+
+	if (!Blackboard)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	UStudentPerceptor* Perceptor =  Pawn->FindComponentByClass<UStudentPerceptor>();
+
+	if (!Perceptor)
+	{
+		return EBTNodeResult::Failed;
+	}
+	
+	const TArray<TObjectPtr<ABaseZombie>>&VisibleZombies = Perceptor->GetVisibleEnemies();
+	ABaseZombie* ClosestEnemy = nullptr;
+	float ClosestDistanceSquared = FLT_MAX;
+
+	for (const TObjectPtr<ABaseZombie>& Zombie : VisibleZombies)
+	{
+		if (!IsValid(Zombie))
+		{
+			continue;
+		}
+
+
+		const float DistanceSquared = FVector::DistSquared2D(Pawn->GetActorLocation(), Zombie->GetActorLocation());
+
+		if (DistanceSquared < ClosestDistanceSquared)
+		{
+			ClosestDistanceSquared = DistanceSquared;
+			ClosestEnemy = Zombie.Get();
+		}
+	}
+	
+	if (!ClosestEnemy)
+	{
+		Blackboard->ClearValue(TargetEnemyKey.SelectedKeyName);
+		return EBTNodeResult::Failed;
+	}
+	
+	Blackboard->SetValueAsObject(TargetEnemyKey.SelectedKeyName, ClosestEnemy);
+
+
+	return EBTNodeResult::Succeeded;
+}
---
+++ Plugins/GameFeatures/VerstraetenCosmoZombie/Source/VerstraetenCosmoZombieRuntime/Public/BTTask_SelectEnemy.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BehaviorTree/BehaviorTreeTypes.h"
+
+#include "BTTask_SelectEnemy.generated.h"
+
+
+UCLASS()
+class VERSTRAETENCOSMOZOMBIERUNTIME_API UBTTask_SelectEnemy : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UBTTask_SelectEnemy();
+
+
+protected:
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector TargetEnemyKey;
+};
