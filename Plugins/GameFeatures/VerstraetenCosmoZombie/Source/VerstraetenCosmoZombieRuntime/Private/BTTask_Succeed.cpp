d6bef0aab0a6c72a3aef5379d4dcdbba331fe7ec
Cosmo-Verstraeten <cosmo.verstraeten@student.howest.be>
2026-08-16 14:53:33 +0200

    added a succeed task that can be used with a blackboard decorator

---
+++ Plugins/GameFeatures/VerstraetenCosmoZombie/Source/VerstraetenCosmoZombieRuntime/Private/BTTask_Succeed.cpp
@@ -0,0 +1,13 @@
+﻿#include "BTTask_Succeed.h"
+
+
+UBTTask_Succeed::UBTTask_Succeed()
+{
+	NodeName = TEXT("Succeed");
+}
+
+
+EBTNodeResult::Type UBTTask_Succeed::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	return EBTNodeResult::Succeeded;
+}
---
+++ Plugins/GameFeatures/VerstraetenCosmoZombie/Source/VerstraetenCosmoZombieRuntime/Public/BTTask_Succeed.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_Succeed.generated.h"
+
+
+UCLASS()
+class VERSTRAETENCOSMOZOMBIERUNTIME_API UBTTask_Succeed
+	: public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UBTTask_Succeed();
+
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
