e2258f39e12c70950b60810c3008798cf7f2af44
Cosmo-Verstraeten <cosmo.verstraeten@student.howest.be>
2026-08-16 14:56:45 +0200

    added a task that selects the closest unlooted house of the known houses

---
+++ Plugins/GameFeatures/VerstraetenCosmoZombie/Source/VerstraetenCosmoZombieRuntime/Private/BTTask_SelectHouse.cpp
@@ -0,0 +1,126 @@
+﻿#include "BTTask_SelectHouse.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Pawn.h"
+
+#include "StudentPerceptor.h"
+
+
+UBTTask_SelectHouse::UBTTask_SelectHouse()
+{
+	NodeName = TEXT("Select House");
+
+
+	TargetHouseKey.AddObjectFilter(
+		this,
+		GET_MEMBER_NAME_CHECKED(
+			UBTTask_SelectHouse,
+			TargetHouseKey),
+		AActor::StaticClass());
+
+
+	AtTargetHouseKey.AddBoolFilter(
+		this,
+		GET_MEMBER_NAME_CHECKED(
+			UBTTask_SelectHouse,
+			AtTargetHouseKey));
+}
+
+
+EBTNodeResult::Type UBTTask_SelectHouse::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController* AIController = OwnerComp.GetAIOwner();
+
+	if (!AIController)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+
+	APawn* Pawn =AIController->GetPawn();
+
+	if (!Pawn)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+
+	UBlackboardComponent* Blackboard =OwnerComp.GetBlackboardComponent();
+
+	if (!Blackboard)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+
+	UStudentPerceptor* Perceptor = Pawn->FindComponentByClass<UStudentPerceptor>();
+
+	if (!Perceptor)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	AActor* CurrentTargetHouse =
+		Cast<AActor>(Blackboard->GetValueAsObject( TargetHouseKey.SelectedKeyName));
+
+
+	if (CurrentTargetHouse && !Perceptor->IsHouseSearched(CurrentTargetHouse))
+	{
+		return EBTNodeResult::Succeeded;
+	}
+	
+
+	const TArray<TObjectPtr<AActor>>& KnownHouses = Perceptor->GetKnownHouses();
+
+
+	AActor* BestHouse = nullptr;
+
+	float BestDistanceSquared = FLT_MAX;
+
+
+	for (const TObjectPtr<AActor>& KnownHouse : KnownHouses)
+	{
+		AActor* House = KnownHouse.Get();
+
+
+		if (!IsValid(House))
+		{
+			continue;
+		}
+
+
+		if (Perceptor->IsHouseSearched(House))
+		{
+			continue;
+		}
+
+
+		const float DistanceSquared = FVector::DistSquared2D(Pawn->GetActorLocation(), House->GetActorLocation());
+
+
+		if (DistanceSquared < BestDistanceSquared)
+		{
+			BestDistanceSquared = DistanceSquared;
+
+			BestHouse = House;
+		}
+	}
+
+	if (!BestHouse)
+	{
+		Blackboard->ClearValue(TargetHouseKey.SelectedKeyName);
+
+		Blackboard->SetValueAsBool(AtTargetHouseKey.SelectedKeyName,false);
+
+		return EBTNodeResult::Failed;
+	}
+	
+	
+	Blackboard->SetValueAsObject(TargetHouseKey.SelectedKeyName, BestHouse);
+
+	Blackboard->SetValueAsBool(AtTargetHouseKey.SelectedKeyName,false);
+
+
+	return EBTNodeResult::Succeeded;
+}
---
+++ Plugins/GameFeatures/VerstraetenCosmoZombie/Source/VerstraetenCosmoZombieRuntime/Public/BTTask_SelectHouse.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BehaviorTree/BehaviorTreeTypes.h"
+#include "BTTask_SelectHouse.generated.h"
+
+
+UCLASS()
+class VERSTRAETENCOSMOZOMBIERUNTIME_API UBTTask_SelectHouse
+	: public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UBTTask_SelectHouse();
+
+
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector TargetHouseKey;
+	
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector AtTargetHouseKey;
+};
