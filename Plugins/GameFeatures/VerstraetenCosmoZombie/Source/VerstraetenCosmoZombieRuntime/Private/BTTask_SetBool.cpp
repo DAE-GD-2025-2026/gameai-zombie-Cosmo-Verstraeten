a99905877585920324e0e84604ecb5177b6485b8
Cosmo-Verstraeten <cosmo.verstraeten@student.howest.be>
2026-08-16 14:52:11 +0200

    added setbool task to be able to use in the tree

---
+++ Plugins/GameFeatures/VerstraetenCosmoZombie/Source/VerstraetenCosmoZombieRuntime/Private/BTTask_SetBool.cpp
@@ -0,0 +1,33 @@
+﻿#include "BTTask_SetBool.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
+
+
+UBTTask_SetBool::UBTTask_SetBool()
+{
+	NodeName = TEXT("Set Blackboard Bool");
+
+	BoolKey.AddBoolFilter(
+		this,
+		GET_MEMBER_NAME_CHECKED(
+			UBTTask_SetBool,
+			BoolKey));
+}
+
+
+EBTNodeResult::Type UBTTask_SetBool::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	UBlackboardComponent* Blackboard = OwnerComp.GetBlackboardComponent();
+
+
+	if (!Blackboard)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+
+	Blackboard->SetValueAsBool(BoolKey.SelectedKeyName, Value);
+
+
+	return EBTNodeResult::Succeeded;
+}
---
+++ Plugins/GameFeatures/VerstraetenCosmoZombie/Source/VerstraetenCosmoZombieRuntime/Public/BTTask_SetBool.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BehaviorTree/BehaviorTreeTypes.h"
+#include "BTTask_SetBool.generated.h"
+
+
+UCLASS()
+class VERSTRAETENCOSMOZOMBIERUNTIME_API UBTTask_SetBool
+	: public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UBTTask_SetBool();
+
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector BoolKey;
+
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	bool Value{ true };
+};
