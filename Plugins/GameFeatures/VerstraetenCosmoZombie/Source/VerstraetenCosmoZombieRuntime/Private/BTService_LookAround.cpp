2845602f3fc9e2e9b755102e92b59033e11f8b7c
Cosmo-Verstraeten <cosmo.verstraeten@student.howest.be>
2026-08-16 23:56:07 +0200

    added a service that track health/stamina

---
+++ Plugins/GameFeatures/VerstraetenCosmoZombie/Source/VerstraetenCosmoZombieRuntime/Private/BTService_LookAround.cpp
@@ -0,0 +1,92 @@
+﻿#include "BTService_LookAround.h"
+
+#include "AIController.h"
+#include "GameFramework/Pawn.h"
+
+#include "SurvivorLookAround.h"
+
+
+UBTService_LookAround::UBTService_LookAround()
+{
+	NodeName = TEXT("Look Around");
+
+	bNotifyBecomeRelevant = true;
+	bNotifyCeaseRelevant = true;
+	
+	bNotifyTick = false;
+	
+}
+
+void UBTService_LookAround::OnBecomeRelevant(UBehaviorTreeComponent& OwnerComp,uint8* NodeMemory)
+{
+	Super::OnBecomeRelevant(OwnerComp, NodeMemory);
+
+	UE_LOG(
+			LogTemp,
+			Warning,
+			TEXT("LOOK AROUND SERVICE STARTED"));
+	AAIController* AIController = OwnerComp.GetAIOwner();
+
+	if (!AIController)
+	{
+		return;
+	}
+	
+	APawn* Pawn = AIController->GetPawn();
+
+	if (!Pawn)
+	{
+		return;
+	}
+	
+	USurvivorLookAround* LookAround = Pawn->FindComponentByClass<USurvivorLookAround>();
+
+	if (!LookAround)
+	{
+		UE_LOG(
+			LogTemp,
+			Error,
+			TEXT("LookAround: COMPONENT NOT FOUND"));
+
+		return;
+	}
+
+
+	UE_LOG(
+		LogTemp,
+		Warning,
+		TEXT("LookAround: COMPONENT FOUND"));
+	LookAround->StartScanning();
+}
+
+void UBTService_LookAround::OnCeaseRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	Super::OnCeaseRelevant(OwnerComp, NodeMemory);
+
+
+	AAIController* AIController = OwnerComp.GetAIOwner();
+
+	if (!AIController)
+	{
+		return;
+	}
+
+
+	APawn* Pawn = AIController->GetPawn();
+
+	if (!Pawn)
+	{
+		return;
+	}
+	
+	USurvivorLookAround* LookAround = Pawn->FindComponentByClass<USurvivorLookAround>();
+
+	if (!LookAround)
+	{
+		return;
+	}
+
+
+
+	LookAround->StopScanning();
+}
---
+++ Plugins/GameFeatures/VerstraetenCosmoZombie/Source/VerstraetenCosmoZombieRuntime/Private/BTService_UpdateNeeds.cpp
@@ -0,0 +1,69 @@
+﻿#include "BTService_UpdateNeeds.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "SurvivorInventoryLogic.h"
+
+
+UBTService_UpdateNeeds::UBTService_UpdateNeeds()
+{
+	NodeName = TEXT("Update Survivor Needs");
+	bNotifyTick = true;
+
+	Interval = 0.25f;
+	RandomDeviation = 0.f;
+	
+	NeedsMedkitKey.AddBoolFilter(
+		this,
+		GET_MEMBER_NAME_CHECKED(
+			UBTService_UpdateNeeds,
+			NeedsMedkitKey));
+
+
+	NeedsFoodKey.AddBoolFilter(
+		this,
+		GET_MEMBER_NAME_CHECKED(
+			UBTService_UpdateNeeds,
+			NeedsFoodKey));
+}
+
+void UBTService_UpdateNeeds::TickNode(UBehaviorTreeComponent& OwnerComp,uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(
+		OwnerComp,
+		NodeMemory,
+		DeltaSeconds);
+	
+	AAIController* Controller = OwnerComp.GetAIOwner();
+	if (!Controller)
+	{
+		return;
+	}
+
+	APawn* Pawn = Controller->GetPawn();
+
+	if (!Pawn)
+	{
+		return;
+	}
+	
+	UBlackboardComponent* Blackboard = OwnerComp.GetBlackboardComponent();
+
+	if (!Blackboard)
+	{
+		return;
+	}
+	
+	USurvivorInventoryLogic* InventoryLogic = Pawn->FindComponentByClass<USurvivorInventoryLogic>();
+
+	if (!InventoryLogic)
+	{
+		return;
+	}
+	const bool bNeedsMedkit = InventoryLogic->ShouldUseMedkit() && InventoryLogic->FindUsableMedkitSlot() != INDEX_NONE;
+	const bool bNeedsFood =InventoryLogic->ShouldUseFood() && InventoryLogic->FindUsableFoodSlot() != INDEX_NONE;
+	
+	Blackboard->SetValueAsBool(NeedsMedkitKey.SelectedKeyName, bNeedsMedkit);
+	
+	Blackboard->SetValueAsBool(NeedsFoodKey.SelectedKeyName, bNeedsFood);
+}
