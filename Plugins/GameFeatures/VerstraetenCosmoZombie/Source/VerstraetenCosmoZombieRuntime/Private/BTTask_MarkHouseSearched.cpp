b24d9d8e8f864701b7ae969fff3b9b89a0faadf4
Cosmo-Verstraeten <cosmo.verstraeten@student.howest.be>
2026-08-16 14:58:40 +0200

    added a task that marks a house as searched

---
+++ Plugins/GameFeatures/VerstraetenCosmoZombie/Source/VerstraetenCosmoZombieRuntime/Private/BTTask_MarkHouseSearched.cpp
@@ -0,0 +1,96 @@
+﻿#include "BTTask_MarkHouseSearched.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Pawn.h"
+
+#include "StudentPerceptor.h"
+
+
+UBTTask_MarkHouseSearched::UBTTask_MarkHouseSearched()
+{
+	NodeName = TEXT("Mark House Searched");
+
+
+	TargetHouseKey.AddObjectFilter(
+		this,
+		GET_MEMBER_NAME_CHECKED(
+			UBTTask_MarkHouseSearched,
+			TargetHouseKey),
+		AActor::StaticClass());
+
+
+	AtTargetHouseKey.AddBoolFilter(
+		this,
+		GET_MEMBER_NAME_CHECKED(
+			UBTTask_MarkHouseSearched,
+			AtTargetHouseKey));
+
+
+	TargetItemKey.AddObjectFilter(
+		this,
+		GET_MEMBER_NAME_CHECKED(
+			UBTTask_MarkHouseSearched,
+			TargetItemKey),
+		AActor::StaticClass());
+}
+
+
+EBTNodeResult::Type UBTTask_MarkHouseSearched::ExecuteTask(
+	UBehaviorTreeComponent& OwnerComp,
+	uint8* NodeMemory)
+{
+	AAIController* AIController = OwnerComp.GetAIOwner();
+
+	if (!AIController)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+
+	APawn* Pawn = AIController->GetPawn();
+
+	if (!Pawn)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+
+	UBlackboardComponent* Blackboard =OwnerComp.GetBlackboardComponent();
+
+	if (!Blackboard)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+
+	UStudentPerceptor* Perceptor = Pawn->FindComponentByClass<UStudentPerceptor>();
+
+	if (!Perceptor)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+
+	AActor* House =Cast<AActor>(Blackboard->GetValueAsObject(TargetHouseKey.SelectedKeyName));
+
+
+	if (!House)
+	{
+		return EBTNodeResult::Failed;
+	}
+	
+	Perceptor->MarkHouseSearched(House);
+
+	Blackboard->SetValueAsBool(
+	TEXT("HasKnownHouse"),
+	Perceptor->HasUnsearchedHouse());
+
+	Blackboard->ClearValue(TargetHouseKey.SelectedKeyName);
+
+	Blackboard->SetValueAsBool(AtTargetHouseKey.SelectedKeyName,false);
+
+	Blackboard->ClearValue(TargetItemKey.SelectedKeyName);
+	
+	return EBTNodeResult::Succeeded;
+}
---
+++ Plugins/GameFeatures/VerstraetenCosmoZombie/Source/VerstraetenCosmoZombieRuntime/Public/BTTask_MarkHouseSearched.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BehaviorTree/BehaviorTreeTypes.h"
+
+#include "BTTask_MarkHouseSearched.generated.h"
+
+
+UCLASS()
+class VERSTRAETENCOSMOZOMBIERUNTIME_API UBTTask_MarkHouseSearched
+	: public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UBTTask_MarkHouseSearched();
+
+
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector TargetHouseKey;
+
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector AtTargetHouseKey;
+
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector TargetItemKey;
+};
